cd317346ec3c0782155dbf46e4f5874857f719f6
marshall <marshall@marshalls-MacBook-Air.local>
2019-01-19 14:21:34 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route } from "react-router-dom";
-import {Container} from './App_css.js'
-import MonsterView from "./components/monsterView/monsterView.js"
+import {Container} from './App_css.js';
+import MonsterView from "./components/monsterView/monsterView.js";
+import SignUp from "./components/authentication/SignUp";
 import Footer from "./components/footer/footer.js";
 import Nav from './components/nav/nav.js';
 import GenGroup from "./components/genGroup/genGroup.js";
@@ -20,13 +21,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Route exact path='/register' component={SignUp} />
         <Container>
-        <Route path='/' component={Nav} />
-         <Route exact path='/' component={GenGroup} />
-         <Route exact path='/compendium' component={MonsterView} />
-          </Container>
+          <Route exact path='/' component={Nav} />
+          <Route exact path='/' component={GenGroup} />
+          <Route exact path='/compendium' component={MonsterView} />
+        </Container>
         <Route path='/' component={Footer} />
-        </div>
+      </div>
     )
   }
 }
