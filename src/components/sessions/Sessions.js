5f16950a7edd545df0954496e758333815d06f96
marshall <marshall@marshalls-MacBook-Air.local>
2019-01-25 10:30:43 -0800

    sessions init

---
+++ src/components/sessions/Sessions.js
@@ -0,0 +1,37 @@
+import React from 'react';
+//import axios from 'axios';
+
+class Sessions extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount(){
+
+	}
+
+	// extra in case you need to reference
+
+	// handleChange = event => {
+ 	//    this.setState({[event.target.name]: event.target.value})
+ 	//  }
+
+	//  <form>
+	// 	<input
+	// 		type="text"
+	// 		placeholder='example'
+	// 		onChange={this.handleChange}
+	// 		name="example"
+	// 		value={this.state.example}
+	// 	/>
+	// </form>
+
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default Sessions;
