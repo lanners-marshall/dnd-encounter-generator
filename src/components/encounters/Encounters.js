2f4b18ee1822d566fbf461021660cc187e627472
marshall <marshall@marshalls-MacBook-Air.local>
2019-01-30 20:21:06 -0800

    update for encounters display

--- src/components/encounters/Encounters.js
+++ src/components/encounters/Encounters.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import {} from './encounters_css.js';
+import './css/style.css'
 
 
 class Encounter extends React.Component {
@@ -27,30 +28,44 @@ class Encounter extends React.Component {
 
 	render() {
 		return (
-			<div>
-				{this.state.monsters.map(m => {
-					return <div>
-						<img src={m.img_url} />
-						<p>{m.name}</p>
-						<p>{m.type}</p>
-						<p>{m.size}</p>
-						<p>{m.count}</p>
-						<p>{m.armorClass}</p>
-						<p>{m.hitPoints}</p>
-						<p>{m.Speed}</p>
-						<p>{m.STR}: ({m.STR_mod})</p>
-						<p>{m.DEX}: ({m.DEX_mod})</p>
-						<p>{m.CON}: ({m.CON_mod})}</p>
-						<p>{m.INT}: ({m.INT_mod})</p>
-						<p>{m.WIS}: ({m.WIS_mod})</p>
-						<p>{m.WIS}: ({m.WIS_mod})</p>
-						<p>{m.CHA} ({m.CHA_mod})</p>
-						<p>{m.xp}</p>
-						<p>{m.challenge_rating}</p>
-						<p>{m.page}</p>
-					</div>
+
+
+<div>
+
+  <div class="wrapper">
+  <div class="card_1s">
+			{this.state.monsters.map(m => {
+					return  <div class=" card_1 [ is-collapsed ] ">
+										<div class="card_1__inner [ js-expander ]">
+											<img src={m.img_url} className="img-div" />
+											<p>{m.name}</p>
+											<p>{m.type}</p>
+											<p>{m.size}</p>
+											<p>{m.count}</p>
+											<p>{m.armorClass}</p>
+											<p>{m.hitPoints}</p>
+											<p>{m.Speed}</p>
+											<p>{m.STR}: ({m.STR_mod})</p>
+											<p>{m.DEX}: ({m.DEX_mod})</p>
+											<p>{m.CON}: ({m.CON_mod})}</p>
+											<p>{m.INT}: ({m.INT_mod})</p>
+											<p>{m.WIS}: ({m.WIS_mod})</p>
+											<p>{m.WIS}: ({m.WIS_mod})</p>
+											<p>{m.CHA} ({m.CHA_mod})</p>
+											<p>{m.xp}</p>
+											<p>{m.challenge_rating}</p>
+											<p>{m.page}</p>
+										</div>
+										<i class="fa fa-folder-o"></i>
+										<div class="card_1__expander">
+											<i class="fa fa-close [ js-collapser ]"></i>
+											Expander
+										</div>
+									</div>
 				})}
 			</div>
+		</div>
+		</div>
 		)
 	}
 }
